2fa2719406d70216a6e5d2a79610d2eaa68b5418
Micael Bittencourt <ti.micaelbittencourt@outlook.com>
2019-02-06 11:50:30 -0200

    Inicio do projeto, foi criado a classe Account e a DomainException

---
+++ ConsoleAppAccount/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConsoleAppAccount.Entities;
+namespace ConsoleAppAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter account data: ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Balance: ");
+            double balance = double.Parse(Console.ReadLine());
+            Console.Write("Withdraw Limit: ");
+            double withdraw = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amountWithdraw = double.Parse(Console.ReadLine());
+            
+            Account account = new Account(number, holder,balance,withdraw);
+
+            account.WithDraw(amountWithdraw);
+
+            Console.WriteLine(account);
+        }
+    }
+}
